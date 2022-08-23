dd83c6d7a8f06b3ce6f660b19a8527f2cf467c73
pct-cclausen <c.clausen@pct-digital.de>
2022-08-23 13:58:26 +0200

    4 files modified

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as jose from 'jose';
 
 export interface HighScoreEntry {
   name: string;
@@ -14,45 +15,85 @@ export interface ScanEvent {
 export interface LocalQRCodes {
   id: number;
   description: string;
+  points: number;
 }
 
 @Injectable({ providedIn: 'root' })
 export class BackendService {
-  // used during development
+  // used during development, in production the app will need a real backend
   localScanEvents: ScanEvent[] = [];
   localCodes: LocalQRCodes[] = [];
 
   isDevEnvironment() {
     return (
       window.location.href.indexOf('localhost') !== -1 ||
+      window.location.href.indexOf('127.0.0.1') !== -1 ||
       window.location.href.indexOf('stackblitz') !== -1
     );
   }
 
-  getHighscores(): HighScoreEntry[] {
+  async getHighscores(): Promise<HighScoreEntry[]> {
     if (!this.isDevEnvironment()) {
       throw new Error('Implement a real backend for production!');
     } else {
-      return [];
+      const byName: Record<string, number> = {};
+      for (const event of this.localScanEvents) {
+        byName[event.groupName] = (byName[event.groupName] || 0) + 1;
+      }
+
+      const highscores: HighScoreEntry[] = Object.keys(byName).map((name) => ({
+        name,
+        points: byName[name],
+      }));
+
+      highscores.sort((a, b) => b.points - a.points);
+
+      return highscores;
     }
   }
 
-  addPoints(jwtScanned: string, groupName: string) {
+  async addPoints(jwtScanned: string, groupName: string): Promise<number> {
     if (!this.isDevEnvironment()) {
       throw new Error('Implement a real backend for production!');
     } else {
+      // in dev mode no verification is done
+      // in production the verification will happen in the server
+      // for size reasons the jwt does not contain anything but the id
+      const claims = jose.decodeJwt(jwtScanned);
+      const id = Number(claims.jti);
+      const localCode = this.localCodes.find((lc) => lc.id === id);
+      if (localCode != null) {
+        this.localScanEvents.push({
+          groupName,
+          points: localCode.points,
+          qrId: localCode.id,
+        });
+        return localCode.points;
+      }
     }
+    return 0;
   }
 
-  storeQRCodeDescription(description: string) {
+  async storeQRCodeDescription(description: string, points: number) {
     if (!this.isDevEnvironment()) {
-      throw new Error('Implement a real backend for production!0');
+      throw new Error('Implement a real backend for production!');
     } else {
       this.localCodes.push({
         id: this.localCodes.length + 1,
         description,
+        points,
       });
       return this.localCodes.length;
     }
+  }
+
+  async generateQRCode(id: number, key: string) {
+    const jwt = await new jose.SignJWT({
+      jti: id + '',
+    })
+      .setProtectedHeader({ alg: 'HS256' })
+      .sign(new TextEncoder().encode(key));
+
+    return jwt;
   }
 }
--- src/app/menu/create/create-code/create-code.component.ts
+++ src/app/menu/create/create-code/create-code.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../../backend.service';
 
 @Component({
   selector: 'app-create-code',
   templateUrl: './create-code.component.html',
-  styleUrls: ['./create-code.component.css']
+  styleUrls: ['./create-code.component.css'],
 })
 export class CreateCodeComponent implements OnInit {
+  password = '';
+  description = '';
+  points = 0;
 
-  constructor() { }
+  constructor(private backend: BackendService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async generate() {
+    if (
+      this.password.length > 0 &&
+      this.description.length > 0 &&
+      this.points > 0
+    ) {
+      const id = await this.backend.storeQRCodeDescription(
+        this.description,
+        this.points
+      );
+      const jwt = await this.backend.generateQRCode(id, this.password);
+    }
   }
-
 }
